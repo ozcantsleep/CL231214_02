52dce91a0024bf7dbc5c1ccdd234fa59f49c26bd
Jun Ahn <brigdesatis@gmail.com>
2023-12-14 18:11:55 +0900

    231214-002

---
+++ Source/CL231214_02/CL231214_02.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "CL231214_02.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CL231214_02, "CL231214_02" );
